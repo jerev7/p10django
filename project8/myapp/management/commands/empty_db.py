a5fec5975d6b9a9c6699c15b10e555b77619c8c0
jerev7 <verdet.jeremie@gmail.com>
2021-09-01 19:55:07 +0200

    added a command to empty database

---
+++ project8/myapp/management/commands/empty_db.py
@@ -0,0 +1,15 @@
+from myapp.models import Category, Products
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    help = "Delete all data from tables"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+    	Products.objects.all().delete()
+    	Category.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS('All data from Products and Category has been deleted'))
