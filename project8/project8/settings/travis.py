58d66c0a20b36a6c713526de373e7eea06ec8442
jerev7 <verdet.jeremie@gmail.com>
2021-09-04 00:06:18 +0200

    got back to previous travis.py

--- project8/project8/settings/travis.py
+++ project8/project8/settings/travis.py
@@ -10,14 +10,3 @@ DATABASES = {
         'PORT': ''
     }
 }
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = ('whitenoise.storage.'
-                       'CompressedManifestStaticFilesStorage')
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
