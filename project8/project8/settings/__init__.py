09d4899b2df67908859810c729cbe8200ab6a7d4
jerev7 <verdet.jeremie@gmail.com>
2021-09-03 16:49:03 +0200

    added server db in init

--- project8/project8/settings/__init__.py
+++ project8/project8/settings/__init__.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'project8.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project8',
-        'USER': 'jeremiev',
-        'PASSWORD': '',
+        'NAME': 'p9db',
+        'USER': 'user1',
+        'PASSWORD': 'newSally_9',
         'HOST': '',
         'PORT': '5432'
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
