8cef4ceb08cc58e20b685aa0addf8d3a7738e683
jerev7 <verdet.jeremie@gmail.com>
2021-09-03 23:59:32 +0200

    added some code in travis.py

--- project8/project8/settings/__init__.py
+++ project8/project8/settings/__init__.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'project8.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'project8',
-#         'USER': 'jeremiev',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project8',
+        'USER': 'jeremiev',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
